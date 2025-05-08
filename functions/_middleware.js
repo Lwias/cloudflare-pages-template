6426acf1a3ce74bba3119706f9d623a6b92cc949
Lwias <71401424+Lwias@users.noreply.github.com>
2025-05-08 23:33:52 +0300

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -18,29 +18,29 @@ export async function onRequest(context) {
   }
 
   // Metatags Variables (edit these!)
-  const metatitle = "Cloudflare Pages Template"
-  const metadescription = "Get started with Cloudflare Pages by deploying this pretty simple template on your own account, and start playing around."
+  const metatitle = "Eras Hukuk ve Danışmanlık"
+  const metadescription = "Ankara merkezli Eras Hukuk Bürosu, ceza hukuku, ticaret hukuku ve aile hukuku alanlarında uzman avukatlık ve danışmanlık hizmetleri sunar."
 
   let name = searchParams.get('myQuery')
   let ogtag
 
-  // these are the metatags we want to inject into the site
+  // Meta tag HTML
   ogtag = `
     <meta property="og:title" content="${metatitle}" />
     <meta property="og:description" content="${metadescription}" />
-    <meta property="og:locale" content="en_US" />
+    <meta property="og:locale" content="tr_TR" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="${request.url}" />
-    <meta property="og:image" content="${request.url}img/preview.png?${name ? 'myQuery=' + name : 'default'}" />
-
+    <meta property="og:image" content="${request.url}img/eraslaww.png?${name ? 'myQuery=' + name : 'default'}" />
     <meta property="og:image:height" content="630" />
     <meta property="og:image:width" content="1200" />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="${metatitle}" />
     <meta name="twitter:description" content="${metadescription}" />
+    <meta name="twitter:image" content="${request.url}img/eraslaww.png?${name ? 'myQuery=' + name : 'default'}" />
 
-    <meta name="description" content="and even more stuff about my page" />
+    <meta name="description" content="${metadescription}" />
   `
 
   return new HTMLRewriter().on('head', new ElementHandler(ogtag)).transform(res)
